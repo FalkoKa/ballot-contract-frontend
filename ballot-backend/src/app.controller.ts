9699c63986f8f6161369946ed62d64ef341faeb4
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-11-18 11:37:25 +0100

    implement MTK functions

--- ballot-backend/src/app.controller.ts
+++ ballot-backend/src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('last-block-number')
+  async getLastBlockNumber(): Promise<string> {
+    return await this.appService.getLastBlockNumber();
+  }
+
+  @Get('token-address')
+  getContractAddress() {
+    return { result: this.appService.getTokenAddress() };
+  }
+
+  @Get('token-name')
+  async getTokenName() {
+    return { result: await this.appService.getTokenName() };
+  }
+
+  @Get('total-supply')
+  async getTotalSupply() {
+    return { result: await this.appService.getTotalSupply() };
+  }
+
+  @Get('token-balance/:address')
+  async getTokenBalance(@Param('address') address: string) {
+    return { result: await this.appService.getTokenBalance(address) };
+  }
+
+  @Get('transaction-receipt')
+  async getTransactionReceipt(@Query('hash') hash: string) {
+    return { result: await this.appService.getTransactionReceipt(hash) };
   }
 }
--- ballot-backend/src/app.service.ts
+++ ballot-backend/src/app.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
+import * as tokenJson from './assets/MyToken.json';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  contract: ethers.Contract;
+  provider: ethers.Provider;
+  wallet: ethers.Wallet;
+
+  constructor(private configService: ConfigService) {
+    this.provider = new ethers.JsonRpcProvider(
+      this.configService.get<string>('RPC_ENDPOINT_URL'),
+    );
+    this.wallet = new ethers.Wallet(
+      this.configService.get<string>('PRIVATE_KEY'),
+      this.provider,
+    );
+    this.contract = new ethers.Contract(
+      this.configService.get<string>('TOKEN_ADDRESS'),
+      tokenJson.abi,
+      this.wallet,
+    );
+  }
+
+  async getLastBlockNumber(): Promise<string> {
+    const provider = ethers.getDefaultProvider('sepolia');
+    const blockNumber = await provider.getBlockNumber();
+    return `The last block number from Sepolia was ${blockNumber}`;
+  }
+
+  getTokenAddress(): string {
+    return this.configService.get<string>('TOKEN_ADDRESS');
+  }
+
+  async getTokenName(): Promise<string> {
+    return await this.contract.name();
+  }
+
+  async getTotalSupply() {
+    return ethers.formatUnits(await this.contract.totalSupply());
+  }
+
+  async getTokenBalance(address: string) {
+    const balance = await this.contract.balanceOf(address);
+    return ethers.formatUnits(balance);
+  }
+
+  async getTransactionReceipt(hash: string) {
+    return await this.provider.getTransaction(hash);
   }
 }
