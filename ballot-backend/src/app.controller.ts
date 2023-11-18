fbb6510735aadb8f67da5c637cecf488e9c9d71f
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-11-18 13:14:54 +0100

    wip

--- ballot-backend/src/app.controller.ts
+++ ballot-backend/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MintTokenDto } from './dtos/mintToken.dto';
+import { DelegateVotesDto } from './dtos/delegateVotes.dto';
 
 @Controller()
 export class AppController {
@@ -33,5 +35,31 @@ export class AppController {
   @Get('transaction-receipt')
   async getTransactionReceipt(@Query('hash') hash: string) {
     return { result: await this.appService.getTransactionReceipt(hash) };
+  }
+
+  @Get('server-wallet-address')
+  getServerWalletAddress() {
+    return { result: this.appService.getServerWalletAddress() };
+  }
+
+  @Get('check-minter-role')
+  async checkMinterRole(@Query('address') address: string) {
+    return { result: await this.appService.checkMinterRole(address) };
+  }
+
+  @Post('mint-tokens')
+  async mintTokens(@Body() body: MintTokenDto) {
+    return { result: await this.appService.mintTokens(body.address) };
+  }
+
+  @Post('transfer-tokens')
+  async transferTokens(@Body() body: any) {
+    // todo
+    return await this.appService.transferTokens();
+  }
+
+  @Post('delegate-votes')
+  async deletateVotes(@Body() body: DelegateVotesDto) {
+    return await this.appService.delegateVotes(body.address);
   }
 }
--- ballot-backend/src/app.service.ts
+++ ballot-backend/src/app.service.ts
@@ -3,6 +3,8 @@ import { ethers } from 'ethers';
 import * as tokenJson from './assets/MyToken.json';
 import { ConfigService } from '@nestjs/config';
 
+const MINT_VALUE = ethers.parseUnits('1');
+
 @Injectable()
 export class AppService {
   contract: ethers.Contract;
@@ -49,5 +51,46 @@ export class AppService {
 
   async getTransactionReceipt(hash: string) {
     return await this.provider.getTransaction(hash);
+  }
+
+  getServerWalletAddress() {
+    return this.wallet.address;
+  }
+
+  async checkMinterRole(address: string) {
+    const minterRole = await this.contract.MINTER_ROLE();
+    const hasRole = await this.contract.hasRole(minterRole, address);
+    return hasRole;
+  }
+
+  async mintTokens(address: string) {
+    try {
+      const mintTx = await this.contract.mint(address, MINT_VALUE);
+      await mintTx.wait();
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async transferTokens() {
+    // todo
+    return null;
+  }
+
+  async delegateVotes(address: string) {
+    try {
+      console.log(address);
+      const delegateTx = await this.contract.delegate(address);
+      await delegateTx.wait();
+
+      const votesAfter = await this.contract.getVotes(address);
+      console.log(votesAfter);
+      return { result: ethers.formatUnits(votesAfter) };
+    } catch (error: any) {
+      console.log(error);
+      return { error: 'There was an error' };
+    }
   }
 }
